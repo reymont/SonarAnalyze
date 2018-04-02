7cd99f5dc15013092235fd84933993b2145b9c00
reymont <258227346@qq.com>
2018-04-02 16:36:08 +0800

    AnalyzeMainTest

---
+++ src/main/java/com/cmi/sonar/AnalyzeMainTest.java
@@ -0,0 +1,10 @@
+package com.cmi.sonar;
+
+public class AnalyzeMainTest {
+
+    public static void main(String[] args) throws Exception {
+        String argss[]={"-?"};
+        // argss = {"-t  1000"};
+        AnalyzeMain.main(argss);
+    }
+}
